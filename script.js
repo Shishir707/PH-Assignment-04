9176d4fe160f65ac623e9e8f68fe42436f7e20f2
Shishir707 <mirshr8@gmail.com>
2026-02-22 16:30:01 +0600

    implement reject button

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ function toggleButton(id) {
     if (id === "all-btn") {
         allBtn.classList.add("bg-blue-500", "text-white");
         filterContent.classList.add("hidden");
-        allJobs.classList.remove("hidden"); 
+        allJobs.classList.remove("hidden");
     } else if (id === "interview-btn") {
         interviewBtn.classList.add("bg-blue-500", "text-white");
         allJobs.classList.add("hidden");
@@ -41,6 +41,8 @@ function toggleButton(id) {
         renderInterviewList();
     } else if (id === "rejected-btn") {
         rejectedBtn.classList.add("bg-blue-500", "text-white");
+        allJobs.classList.add("hidden");
+        filterContent.classList.remove("hidden");
     }
 }
 
@@ -70,6 +72,33 @@ mainContent.addEventListener("click", function (event) {
             totalCount();
             renderInterviewList();
         }
+    } else if (event.target.classList.contains("rejected-btn")) {
+        const parentNode = event.target.parentNode;
+        const JobName = parentNode.querySelector(".job-name").innerText;
+        const JobTitle = parentNode.querySelector(".job-title").innerText;
+        const WorkingHour = parentNode.querySelector(".working-hour").innerText;
+        const Description = parentNode.querySelector(".description").innerText;
+
+        parentNode.querySelector(".state").innerText = "Rejected";
+
+        const state = "Rejected";
+
+        const jobInfo = {
+            JobName,
+            JobTitle,
+            WorkingHour,
+            Description,
+            state
+        }
+
+        console.log(jobInfo);
+
+        const existingJob = rejectedList.find(item => item.JobName === jobInfo.JobName);
+        if (!existingJob) {
+            rejectedList.push(jobInfo);
+            totalCount();
+            renderRejectedList();
+        }
     }
 });
 
@@ -89,12 +118,30 @@ function renderInterviewList() {
 
             <button class="state btn btn-soft btn-primary mb-2">${job.state}</button>
             <p class="description text-sm mb-2">${job.Description}</p>
-            <button class="btn btn-dash btn-success">Interview</button>
-            <button class="btn btn-dash btn-error">Rejected</button>
+            <button class="interview-btn btn btn-dash btn-success">Interview</button>
+            <button class="rejected-btn btn btn-dash btn-error">Rejected</button>
          `
         filterContent.appendChild(jobElement);
     }
+}
 
-
-
+function renderRejectedList() {
+    filterContent.innerHTML = "";
+    for (let job of rejectedList) {
+        let jobElement = document.createElement("div");
+        jobElement.classList.add("mt-8", "p-4", "bg-white");
+        jobElement.innerHTML = `
+        <div class="title flex items-center justify-between mb-2">
+                <h3 class="job-name font-semibold text-blue-950">${job.JobName}</h3>
+                <button class="delete btn btn-soft btn-error"><i class="fa-solid fa-trash-can"></i></button>
+            </div>
+            <p class="job-title mb-2 text-neutral-400">${job.JobTitle}</p>
+            <p class="working-hour mb-2 text-neutral-400">${job.WorkingHour}</p>    
+            <button class="state btn btn-soft btn-primary mb-2">${job.state}</button>
+            <p class="description text-sm mb-2">${job.Description}</p>
+            <button class="interview-btn btn btn-dash btn-success">Interview</button>
+            <button class="rejected-btn btn btn-dash btn-error">Rejected</button>
+            `
+        filterContent.appendChild(jobElement);
+    }
 }
